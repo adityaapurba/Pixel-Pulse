d18540e11b8a68398dd3414c06689e714c4017b3
Aditya <adityasapurba@gmail.com>
2023-10-31 20:23:05 +0530

    commiting cause want to switch

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,12 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -8,25 +16,39 @@ interface Props {
 }
 
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
-    const {data, isLoading, error} = useGenres();
+  const { data, isLoading, error } = useGenres();
 
-
-    if(error) return null;
-    if (isLoading) return <Spinner />;
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
+    <>
+    <Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
     <List>
-    {data.map(genre => (
+      {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
-            <HStack gap={5}>
-                <Image boxSize="40px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)}
-                />
-                <Button fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
-            </HStack>
+          <HStack gap={5}>
+            <Image
+              boxSize="40px"
+              borderRadius={8}
+              objectFit="cover"
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Button
+              whiteSpace="normal"
+              textAlign="left"
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              variant="link"
+            >
+              {genre.name}
+            </Button>
+          </HStack>
         </ListItem>
-        )
-    )}
+      ))}
     </List>
-  )
-}
+    </>
+  );
+};
 
-export default GenreList
+export default GenreList;
